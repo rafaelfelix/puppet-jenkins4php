e47ca1f5c6a282be8c5ff4b4dabf589a9546afd1
Rafael Felix Correa <rafael.felix@gmail.com>
2011-12-30 11:47:18 -0200

    Initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,17 @@
+# Class: jenkins4php
+#
+# This module manages jenkins4php
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# [Remember: No empty lines between comments and class definition]
+class jenkins4php {
+
+
+}
